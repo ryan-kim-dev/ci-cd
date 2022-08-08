da6f2ea42c4d3d5399af1e20b786a304720dba89
ryan-kim-dev <ryankim.h.dev@gmail.com>
2022-08-08 11:52:44 +0900

    테스트케이스 fail시 action 테스트

--- my-agora-states-server/app.js
+++ my-agora-states-server/app.js
@@ -9,17 +9,20 @@ app.use(cors());
 // TODO: Express 내장 미들웨어인 express.json()을 적용합니다.
 app.use(express.json());
 // OPTIONAL: HTTP 요청 logger인 morgan을 적용합니다.
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms'))
+app.use(
+  morgan(':method :url :status :res[content-length] - :response-time ms')
+);
 
 const port = 3001;
 const discussionsRouter = require('./router/discussions');
 
-// TODO: /discussions 경로로 라우팅합니다. 
+// TODO: /discussions 경로로 라우팅합니다.
 app.use('/discussions', discussionsRouter);
 
-app.get('/', (req, res) => {
-  res.status(200).send('Welcome, My Agora States Server!');
-});
+// 테스트케이스 통과시에만 배포되도록 테스트하기 위해 주석처리
+// app.get('/', (req, res) => {
+//   res.status(200).send('Welcome, My Agora States Server!');
+// });
 
 const server = app.listen(port, () => {
   console.log(`[RUN] My Agora States Server... | http://localhost:${port}`);
